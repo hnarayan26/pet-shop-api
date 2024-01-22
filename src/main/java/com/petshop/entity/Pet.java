df80b5403c1d2d9a9214627c73de5816ec85c713
Harshita Narayan <harshita.narayan@booking.com>
2024-01-22 16:44:27 +0000

    Update pet entity

--- src/main/java/com/petshop/entity/Pet.java
+++ src/main/java/com/petshop/entity/Pet.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 @Entity
 @Table(name = "pet")
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class Pet {
 
